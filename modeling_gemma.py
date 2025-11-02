0ce664e1be874cf19646bdeea7ce22c2e1c290c3
Mukul Agarwal <agarwal1993mukul@gmail.com>
2025-11-02 11:37:04 +0530

    modifyint the gemma model

---
+++ modeling_gemma.py
@@ -0,0 +1,92 @@
+import torch
+from torch import nn
+from typing import Optional, Tuple, List
+from torch.nn import CrossEntropyLoss
+import math
+from modeling_siglip import SiglipVisionConfig, SiglipVisionModel
+
+class GemmaConfig():
+     pass
+
+class PaliGemmaConfig():
+
+    def __init__(
+        self,
+        vision_config=None,
+        text_config=None,
+        ignore_index=-100,
+        image_token_index=256000,
+        vocab_size=257152,
+        projection_dim=2048,
+        hidden_size=2048,
+        pad_token_id=None,
+        **kwargs,
+    ):
+        super().__init__()
+        self.ignore_index = ignore_index
+        self.image_token_index = image_token_index
+        self.vocab_size = vocab_size
+        self.projection_dim = projection_dim
+        self.hidden_size = hidden_size
+        self.vision_config = vision_config
+        self.is_encoder_decoder = False
+        self.pad_token_id = pad_token_id
+
+        self.vision_config = SiglipVisionConfig(**vision_config)
+        self.text_config = text_config
+
+        self.text_config = GemmaConfig(**text_config, pad_token_id=pad_token_id)
+        self.vocab_size = self.text_config.vocab_size
+
+        self.text_config.num_image_tokens = (self.vision_config.image_size // self.vision_config.patch_size) ** 2
+        self.vision_config.projection_dim = projection_dim
+     
+
+class PaliGemmaForConditionalGeneration(nn.Module):
+    def __init__(self, config: PaliGemmaConfig):
+        super().__init__()
+        self.config = config
+        self.vision_tower = SiglipVisionModel(config.vision_config)
+        self.multi_modal_projector = PaliGemmaMultiModalProjector(config)
+        self.vocab_size = config.vocab_size
+
+        language_model = GemmaForCasualLLM(config.text_config)
+        self.language_model = language_model
+
+        self.pad_token_id = self.config.pag_token_id if self.config.pad_token_id is not None else -1
+    def tie_weights(self):
+         return self.language_mdoel
+    def forward(
+            self, 
+            input_ids: torch.LongTensor = None,
+            pixel_values : torch.FloatTensor = None,
+            attention_mask : Optional[torch.Tensor] =  None,
+            kv_cache: Optional[KVCache] = None,
+
+    ) -> Tuple:
+            assert torch.all(attention_mask == 1)," The inpyut cannot be padded"
+
+            #1. Extarct the input embedding
+            # Shape : [Batch_size, Seq_Len, Hidden Size]
+            input_embeds = self.language_model.get_input_embeddings()(input_ids)
+
+            #2. Merge text and image
+            # Shapee : [Batch_Size, Channels. Height, Width] -> [Batch_Size, Num_Patches, Embed_Dim]
+            selected_image_feature = self.vision_tower(pixel_values.to(input_embeds.dtype))
+
+            # To resize the image features to of text features
+            # [Batch_Size, Num_Patches, Embed_Dim] -> [Batch_Size, Num_Patches, Hidden_Size]
+            image_features = self.multi_modal_projector(selected_image_feature)
+
+
+            # Merge the embeddings of the text tokens and the image tokens
+            inputs_embeds, attention_mask, position_ids = self._merge_input_ids_with_image_features(image_features, inputs_embeds, input_ids, attention_mask, kv_cache)
+
+            outputs = self.language_model(
+            attention_mask=attention_mask,
+            position_ids=position_ids,
+            inputs_embeds=inputs_embeds,
+            kv_cache=kv_cache,
+        )
+
+            return outputs
